5183a34d833447abb01c2a52648e29f9f9ca4b21
RaffaStone <rafael.dias004@hotmail.com>
2023-08-31 18:04:45 -0300

    Adicionando botão de deletar card ao projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,16 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
+const Time = ( {colaboradores, corPrimaria, corSecundaria, nome, aoDeletar} ) => {
+    const css = { backgroundColor: corSecundaria }
 
     return (
-        (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        (colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+                {colaboradores.map( colaborador => {
+                    return <Colaborador corDeFundo={corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={aoDeletar} />; 
+                })}
             </div>
         </section> 
         : ''
